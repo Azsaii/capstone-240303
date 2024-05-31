626baa68df8e893f62a6f7266a745e6ba6551ed8
cksgud0403 <cksgud0403@naver.com>
2024-05-31 12:41:41 +0900

    fix: 버튼 margin

--- Practice/ProblemDetail.js
+++ Practice/ProblemDetail.js
@@ -9,6 +9,7 @@ import {
   Alert,
   LogBox,
   Dimensions,
+  Platform
 } from 'react-native';
 import {
   collection,
@@ -41,6 +42,7 @@ const styles = StyleSheet.create({
   },
   controlButtonContainer: {
     flexDirection: 'row',
+    marginBottom: 30,
     justifyContent: 'space-between',
   },
   controlButton: {
@@ -344,6 +346,7 @@ const ProblemDetail = ({ route, navigation }) => {
               flexDirection: 'row',
               justifyContent: 'space-between',
               marginBottom: 10,
+              marginTop: Platform.OS === 'ios' ? 60 : 0, // iOS 일 때만 marginTop 적용
             }}
           >
             <Text style={styles.idText}>{formattedId}</Text>
@@ -362,15 +365,16 @@ const ProblemDetail = ({ route, navigation }) => {
           <View style={styles.line} />
           {problems.length > 0 && (
             <>
-              <ScrollView>
-                {problems[currentIndex].data.img && (
-                  <Image
-                    style={{ width: '100%', height: imageSize.height }}
-                    source={{ uri: problems[currentIndex].data.img }}
-                    resizeMode="contain"
-
-                  />
-                )}
+              <ScrollView style={{flex: 1 }} contentContainerStyle={{ flexGrow: 1 }}>
+                <View style={{ flexGrow: 1, justifyContent: 'center', alignItems: 'center' }}>
+                  {problems[currentIndex].data.img && (
+                    <Image
+                      style={{ width: '100%', height: imageSize.height }}
+                      source={{ uri: problems[currentIndex].data.img }}
+                      resizeMode="contain"
+                    />
+                  )}
+                </View>
               </ScrollView>
               <View style={styles.buttonContainer}>
                 {[1, 2, 3, 4, 5].map((number) => (
